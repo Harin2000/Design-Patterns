2e30ba289275177f7edb430d5613af242ad52dff
Harin Dholakia <hdholakia@microsoft.com>
2025-01-19 20:09:04 +0530

    Applied Observer Pattern

--- C#/Observer/Displays/CurrentConditionsDisplay.cs
+++ C#/Observer/Displays/CurrentConditionsDisplay.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace Observer.Displays
 {
-    internal class CurrentConditionsDisplay:IDisplay
+    internal class CurrentConditionsDisplay:IObserver, IDisplayElement
     {
+        // MISSED: Subject should be passed in the constructor of the observer
+        // It's needed to unregister in future.
+        private ISubject weatherData;
+        private float temperature;
+        private float humidity;
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            weatherData = weatherData ?? throw new ArgumentNullException();
+            weatherData.Add(this);
+        }
         public void Update(float temp, float humidity, float pressure)
         {
             Console.WriteLine($"{this.GetType()} received newly updated values: Temperature:{temp}, Humidity:{humidity}, Pressure:{pressure}");
+            this.temperature = temp;
+            this.humidity = humidity;
+            // SOON: Better way to call Display method in MVC pattern
+            Display();
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{this.GetType()} is displaying with current values being: Temperature:{temperature}, Humidity:{humidity}.");
         }
     }
 }
---
+++ C#/Observer/WeatherStation.cs
@@ -0,0 +1,27 @@
+﻿using Observer.Displays;
+
+namespace Observer
+{
+    internal class WeatherStation
+    {
+        static void Main(string[] args)
+        {
+            var weatherData = new WeatherData();
+            //weatherData.MeasurementChanged();
+
+            // After adding observer pattern
+            var currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+            var statisticsDisplay = new StatisticsDisplay(weatherData);
+            var forecastDisplay = new ForecastDisplay(weatherData);
+
+            weatherData.SetMeasurements(1.0f, 1.0f, 1.0f);
+            Console.WriteLine();
+            weatherData.SetMeasurements(2.0f, 2.0f, 2.0f);
+            Console.WriteLine();
+
+            // Adding HeatIndexDisplay
+            var heatIndexDisplay = new HeatIndexDiplay(weatherData);
+            weatherData.SetMeasurements(3.0f, 3.0f, 3.0f);
+        }
+    }
+}
