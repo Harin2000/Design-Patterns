98042c9291a3e273c1b349bb701d15d3b86c8652
Harin Dholakia <hdholakia@microsoft.com>
2025-01-20 09:37:43 +0530

    Completed Decorator

---
+++ C#/Decorator/Condiments/Mocha.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Condiments
+{
+    internal class Mocha:Condiment
+    {
+        public Mocha(Beverage beverage)
+        {
+            Beverage = beverage;
+            SetDescription();
+        }
+        public override int Cost()
+        {
+            return 3 + Beverage.Cost();
+        }
+        protected override void SetDescription()
+        {
+            Description = $"Description of the condiment:{this.GetType()} which is on top of the beverage:\n" +
+                $"{Beverage.Description}\n";
+        }
+    }
+}
--- C#/Decorator/Program.cs
+++ C#/Decorator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Decorator
+﻿using Decorator.Condiments;
+
+namespace Decorator
 {
     internal class Program
     {
@@ -9,7 +11,16 @@
             var drt = new DarkRoast(); Console.WriteLine(drt.Description); cost += drt.Cost();
             var dcf = new Decaf(); Console.WriteLine(dcf.Description); cost += dcf.Cost();
             var esp = new Espresso(); Console.WriteLine(esp.Description); cost += esp.Cost();
-            Console.WriteLine(cost);
+            Console.WriteLine($"Total cost of all the beverages:{cost}"); // 100 + 120 + 140 + 160 = 520
+            Console.WriteLine();
+
+            // After adding condiments.
+            var hbdWithMilk = new SteamedMilk(hbd);
+            var hbdWithMilkAndSoy = new Soy(hbdWithMilk);
+            var hbdWithAllCond = new Mocha(hbdWithMilkAndSoy);
+            Console.WriteLine($"Cost of HouseBlend with StreamedMilk, Soy & Mocha:{hbdWithAllCond.Cost()}"); // 3 + 2 + 1 + 100 = 106
+            Console.WriteLine(hbdWithAllCond.Description);
+            Console.WriteLine();
         }
     }
 }
