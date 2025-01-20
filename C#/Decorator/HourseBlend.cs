613602ed084a053f134f1931f1ed291389157b93
Harin Dholakia <hdholakia@microsoft.com>
2025-01-20 09:08:09 +0530

    Before applying decorator

---
+++ C#/Decorator/HourseBlend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    internal class HourseBlend : Beverage
+    {
+        public HourseBlend() { Description = $"{this.GetType()} description."; }
+        public override int Cost()
+        {
+            return 100;
+        }
+    }
+}
---
+++ C#/Decorator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cost = 0;
+            var hbd = new HourseBlend(); Console.WriteLine(hbd.Description); cost += hbd.Cost();
+            var drt = new DarkRoast(); Console.WriteLine(drt.Description); cost += drt.Cost();
+            var dcf = new Decaf(); Console.WriteLine(dcf.Description); cost += dcf.Cost();
+            var esp = new Espresso(); Console.WriteLine(esp.Description); cost += esp.Cost();
+            Console.WriteLine(cost);
+        }
+    }
+}
