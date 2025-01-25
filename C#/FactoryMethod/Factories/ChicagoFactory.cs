6182c43a642fde10b98c92893e0c9a651628c0b8
Harin Dholakia <hdholakia@microsoft.com>
2025-01-25 18:54:58 +0530

    Implemented SimpleFactoryFranchised

---
+++ C#/FactoryMethod/Factories/ChicagoFactory.cs
@@ -0,0 +1,26 @@
+﻿using SimpleFactoryFranchised.Pizzas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryFranchised.Factories
+{
+    internal class ChicagoFactory    {
+        public IPizza CreatePizza(string type)
+        {
+            switch (type)
+            {
+                case "cheese":
+                    return new ChicagoCheesePizza();
+                case "greek":
+                    return new ChicagoGreekPizza();
+                case "pepperoni":
+                    return new ChicagoPepperoniPizza();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
