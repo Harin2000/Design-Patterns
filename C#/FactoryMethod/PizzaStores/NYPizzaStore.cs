c04e8d75ee906ade5c0ac1ec8a53951e9a78ee0a
Harin Dholakia <hdholakia@microsoft.com>
2025-01-25 19:29:15 +0530

    Implemented Facory Method Pattern

---
+++ C#/FactoryMethod/PizzaStores/NYPizzaStore.cs
@@ -0,0 +1,28 @@
+﻿using FactoryMethod;
+using FactoryMethod.Pizzas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.PizzaStores
+{
+    internal class NYPizzaStore:PizzaStore
+    {
+        public override IPizza CreatePizza(string type)
+        {
+            switch (type)
+            {
+                case "cheese":
+                    return new NYCheesePizza();
+                case "greek":
+                    return new NYGreekPizza();
+                case "pepperoni":
+                    return new NYPepperoniPizza();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ C#/FactoryMethod/PizzaStores/PizzaStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.PizzaStores
+{
+    internal abstract class PizzaStore
+    {
+        public IPizza OrderPizza(string type)
+        {
+            var pizza = CreatePizza(type);
+
+            // This is the issue here, different pizza stores can stop following the standard practice
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+            return pizza;
+        }
+
+        public abstract IPizza CreatePizza(string type);
+    }
+}
