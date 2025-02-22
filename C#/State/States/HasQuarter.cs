1a478093d3eaa34532f1abf1903f1f9943a91e1f
Harin Dholakia <hdholakia@microsoft.com>
2025-02-22 17:44:29 +0530

    Implemented State Pattern

---
+++ C#/State/States/HasQuarter.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace State.States
+{
+    internal class HasQuarter : IState
+    {
+        private GumballMachine gumballMachine;
+        public Random Rand = new Random();
+        public HasQuarter(GumballMachine gumballMachine)
+        {
+            this.gumballMachine = gumballMachine;
+        }
+        public void Dispense()
+        {
+            Console.WriteLine($"Invalid action:{MethodBase.GetCurrentMethod().Name} for state:{GetType()}.");
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine($"Ejected the quarter.");
+            gumballMachine.State = gumballMachine.NoQuarterState;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine($"Invalid action:{MethodBase.GetCurrentMethod().Name} for state:{GetType()}.");
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine($"Turned the crank.");
+            if (Rand.Next(10) == 0)
+            {
+                Console.WriteLine("We got a winner!");
+                gumballMachine.State = gumballMachine.WinnerState;
+            }
+            else
+            {
+                Console.WriteLine("Not a winner.");
+                gumballMachine.State = gumballMachine.SoldState;
+            }
+        }
+    }
+}
