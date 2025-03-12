b9d1a1078176f2f441509d94146853b8be45f72c
Harin Dholakia <hdholakia@microsoft.com>
2025-03-13 01:06:52 +0530

    Implemented Template Method Pattern

---
+++ C#/TemplateMethod/CaffeineBeverage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal abstract class CaffeineBeverage
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            if(CustomerWantsCondiments())
+            AddCondiments();
+        }
+        public void BoilWater() { Console.WriteLine("Boiling water..."); }
+        public abstract void Brew();
+        public void PourInCup() { Console.WriteLine($"Pouring in a cup..."); }
+        public abstract void AddCondiments();
+        public virtual bool CustomerWantsCondiments() { return true; }
+    }
+}
---
+++ C#/TemplateMethod/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal class Coffee : CaffeineBeverage
+    {
+        public override void AddCondiments() { Console.WriteLine($"Executing {MethodBase.GetCurrentMethod().Name} inside {GetType()}"); }
+
+        public override void Brew() { Console.WriteLine($"Executing {MethodBase.GetCurrentMethod().Name} inside {GetType()}"); }
+    }
+}
