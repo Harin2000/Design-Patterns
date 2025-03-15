92e19b579e667ca25678c6a3837a6cb8c2a4f61d
Harin Dholakia <hdholakia@microsoft.com>
2025-03-15 23:20:47 +0530

    Just before "Just when we thought it was safe"

--- C#/Composite/Waitress.cs
+++ C#/Composite/Waitress.cs
@@ -11,26 +11,17 @@ namespace Composite
 {
     internal class Waitress
     {
-        //IEnumerable<MenuItem> panCakeMenu;
-        //IEnumerable<MenuItem> dinerMenu;
-        //IEnumerable<MenuItem> cafeMenu;
         List<IMenu> menus;
         public Waitress(List<IMenu> menus)
         {
-            //this.panCakeMenu = pancakeHouseMenu;
-            //this.dinerMenu = dinerMenu;
-            //this.cafeMenu = cafeMenu;
             this.menus = menus;
         }
         public void PrintMenu()
         {
-            PrintMenus(menus);
-        }
-        private void PrintMenus(List<IMenu> menus)
-        {
             IEnumerator<IMenu> menusEnum = menus.GetEnumerator();
 
-            while (menusEnum.MoveNext()) {
+            while (menusEnum.MoveNext())
+            {
                 IEnumerator<MenuItem> menuEnum = menusEnum.Current.GetEnumerator();
                 PrintMenu(menuEnum);
             }
