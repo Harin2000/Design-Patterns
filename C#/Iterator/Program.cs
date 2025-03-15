b36fba137e95118aca51ccd18b1decd0382258fe
Harin Dholakia <hdholakia@microsoft.com>
2025-03-15 18:34:22 +0530

    Initial State

---
+++ C#/Iterator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var waitress = new Waitress();
+            waitress.PrintMenu();
+        }
+    }
+}
---
+++ C#/Iterator/Waitress.cs
@@ -0,0 +1,33 @@
+﻿using Iterator.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    internal class Waitress
+    {
+        public void PrintMenu()
+        {
+            var panCakeMenu = new PancakeHouseMenu();
+            List<MenuItem> pcMenuItems = panCakeMenu.GetMenuItems();
+            foreach(var item in pcMenuItems)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            var dinerMenu = new DinerMenu();
+            var dinerMenuItems = dinerMenu.GetMenuItems();
+            foreach (var item in dinerMenuItems)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+
+        // NOTE: PrintBreakfastMenu(), PrintLunchMenu(), PrintVegetarianMenu(), IsItemVegetarian(name) will all be a variation to the above
+    }
+}
