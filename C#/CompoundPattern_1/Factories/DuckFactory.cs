4519c08abb50e8aaffc83eb0c2753b2d6c9d3c82
Harin Dholakia <hdholakia@microsoft.com>
2025-03-22 12:45:14 +0530

    Added factory

---
+++ C#/CompoundPattern_1/Factories/DuckFactory.cs
@@ -0,0 +1,32 @@
+﻿using CompoundPattern_1.Quackables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundPattern_1.Factories
+{
+    internal class DuckFactory : IAbstractDuckFactory
+    {
+        public IQuackable CreateDuckCall()
+        {
+            return new DuckCall();
+        }
+
+        public IQuackable CreateMallardDuck()
+        {
+            return new MallardDuck();
+        }
+
+        public IQuackable CreateRedHeadDuck()
+        {
+            return new RedheadDuck();
+        }
+
+        public IQuackable CreateRubberDuck()
+        {
+            return new RubberDuck();
+        }
+    }
+}
