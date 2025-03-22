257e4e329b814de8bca441b8bb259287110d9835
Harin Dholakia <hdholakia@microsoft.com>
2025-03-22 11:58:01 +0530

    Added decorator

--- C#/CompoundPattern_1/DuckSimulator.cs
+++ C#/CompoundPattern_1/DuckSimulator.cs
@@ -12,19 +12,25 @@ namespace CompoundPattern_1
 
         void Simulate()
         {
-            IQuackable mallardDuck = new MallardDuck();
-            IQuackable redheadDuck = new RedheadDuck();
-            IQuackable duckCall = new DuckCall();
-            IQuackable rubberDuck = new RubberDuck();
-            IQuackable gooseDuck = new GooseAdapter(new Goose());
+            //IQuackable mallardDuck = new MallardDuck();
+            //IQuackable redheadDuck = new RedheadDuck();
+            //IQuackable duckCall = new DuckCall();
+            //IQuackable rubberDuck = new RubberDuck();
+            IQuackable mallardDuck = new DuckDecorator(new MallardDuck());
+            IQuackable redheadDuck = new DuckDecorator(new RedheadDuck());
+            IQuackable duckCall = new DuckDecorator(new DuckCall());
+            IQuackable rubberDuck = new DuckDecorator(new RubberDuck());
+            IQuackable gooseDuck = new GooseAdapter(new Goose()); //Don't want honk counts
 
-            Console.WriteLine("\nDuck Simulator:");
+            Console.WriteLine("Duck Simulator:");
 
             Simulate(mallardDuck);
             Simulate(redheadDuck);
             Simulate(duckCall);
             Simulate(rubberDuck);
             Simulate(gooseDuck);
+
+            Console.WriteLine($"Total quacks are:{DuckDecorator.GetQuacks()}");
         }
 
         void Simulate(IQuackable duck)
